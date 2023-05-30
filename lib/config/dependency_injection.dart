fce07cc789c881f0d4b071b07e753879d385b1f3
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-05-31 00:08:02 +0300

    add initModul()

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,19 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:games_organizing/features/out_bording/presentation/controller/out_boarding_controller.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
-
 import '../features/splash/presentation/controller/splash_controller.dart';
 
 final instance = GetIt.instance;
+
+initModul() async {
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
 initSplash() {
   Get.put<SplashController>(
     SplashController(),
   );
 }
 
-finishSplash() {
+disposeSplash() {
   Get.delete<SplashController>();
 }
 
 initOutBoarding() {
-  finishSplash();
+  disposeSplash();
+  Get.put<OutBoarddingController>(OutBoarddingController());
+}
+
+disposeOutBoarding() {
+  Get.delete<OutBoarddingController>();
+}
+
+initLoginView() {
+  disposeOutBoarding();
+  // Get.put<OutBoarddingController>(OutBoarddingController());
 }
