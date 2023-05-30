70dbceaba5fadab5fa9ce16b57f5edf83738ce57
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-05-31 00:07:41 +0300

    add new constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,8 @@
 class Constants {
   static const String baseUrl = "http://example.com";
-  static const double deviceWidth = 375;
-  static const double deviceHeight = 812;
+  static const double deviceWidth = 375.0;
+  static const double deviceHeight = 812.0;
   static const int splashDuration = 3;
+  static const int outBoardingDurationTime = 1;
   static const double elevationAppBar = 0;
 }
