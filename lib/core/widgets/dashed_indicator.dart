e64779e66bfd4d2e513edf99fa5a2fd01d307539
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-05-31 00:09:55 +0300

    modify widget with controller

--- lib/core/widgets/dashed_indicator.dart
+++ lib/core/widgets/dashed_indicator.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:games_organizing/config/constants.dart';
 import 'package:games_organizing/core/resources/manager_color.dart';
+import 'package:games_organizing/features/out_bording/presentation/controller/out_boarding_controller.dart';
+import 'package:get/get.dart';
 import '../resources/managers_size.dart';
 
 Widget dashedIndicator({
   required currentPageIndex,
   required pageCount,
 }) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: List.generate(
-      pageCount,
-      (index) {
-        return Container(
-          width: index == currentPageIndex ? ManagerWidth.w18 : ManagerWidth.w6,
-          height: index == currentPageIndex ? ManagerWidth.w4 : ManagerWidth.w6,
-          margin: EdgeInsets.symmetric(horizontal: ManagerWidth.w4),
-          decoration: BoxDecoration(
-            borderRadius:
-                index == currentPageIndex ? BorderRadius.circular(10) : null,
-            shape: index == currentPageIndex
-                ? BoxShape.rectangle
-                : BoxShape.circle,
-            color: index == currentPageIndex
-                ? ManagerColors.activeColorIndicator
-                : ManagerColors.inactiveColorIndicator,
+  return GetBuilder<OutBoarddingController>(
+      init: OutBoarddingController(),
+      builder: (controller) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            pageCount,
+            (index) {
+              return Container(
+                width: index == currentPageIndex
+                    ? ManagerWidth.w18
+                    : ManagerWidth.w6,
+                height: index == currentPageIndex
+                    ? ManagerWidth.w4
+                    : ManagerWidth.w6,
+                margin: EdgeInsets.symmetric(horizontal: ManagerWidth.w4),
+                decoration: BoxDecoration(
+                  borderRadius: controller.indicatorBorderRadius(index),
+                  shape: controller.indicatorShape(index),
+                  color: controller.indicatorColor(index),
+                ),
+              );
+            },
           ),
         );
-      },
-    ),
-  );
+      });
 }
