05ceef864b34fadf6482b20ddb09192ab038b4ed
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-05-31 00:08:55 +0300

    add new strings

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,4 +1,7 @@
 class ManagerStrings {
   static const String noRouteFound = "No Route Found";
   static const String actHub = "Act-Hub";
+  static const String next = 'Next';
+  static const String startNow = 'Start Now';
+  static const String skip = 'Skip';
 }
