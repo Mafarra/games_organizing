6826ab821d67f7a1251e905975afc0add7facfbf
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-06-03 22:52:17 +0300

    add chooseAccountPage route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,12 +4,14 @@ import 'package:games_organizing/features/splash/presentation/view/splash_view.d
 import 'package:flutter/material.dart';
 
 import '../core/resources/manager_strings.dart';
+import '../features/choose_account/presentation/view/choose_Account.dart';
 import '../features/out_bording/presentation/view/out_boarding_view.dart';
 
 class Routes {
   static const String splashView = '/splash_view';
   static const String outBoardingView = '/out_boarding_view';
   static const String loginView = '/login_view';
+  static const String chooseAccountView = '/choose_account';
 }
 
 class RouteGenerator {
@@ -21,6 +23,8 @@ class RouteGenerator {
       case Routes.outBoardingView:
         initOutBoarding();
         return MaterialPageRoute(builder: (_) => const OutBordingView());
+      case Routes.chooseAccountView:
+        return MaterialPageRoute(builder: (_) => const ChooseAccountView());
       case Routes.loginView:
         initLoginView();
         return MaterialPageRoute(builder: (_) => const LoginView());
