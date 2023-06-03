9d95da71fc1c4aab7a0d5c79cda1253a6c57f774
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-06-03 22:53:06 +0300

    add new pages

---
+++ lib/features/choose_account/presentation/controller/choose_account_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class ChooseAccountController extends GetxController {
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+  }
+
+  @override
+  // TODO: implement onDelete
+  InternalFinalCallback<void> get onDelete => super.onDelete;
+
+  void gotoSigninScreen() {}
+  void gotoSignupScreen() {}
+  void enterAsVisitor() {}
+}
---
+++ lib/features/choose_account/presentation/view/choose_account.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: file_names, duplicate_ignore
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:games_organizing/core/resources/manager_assets.dart';
+import 'package:games_organizing/core/resources/manager_strings.dart';
+import 'package:games_organizing/core/resources/manager_styles.dart';
+import 'package:games_organizing/core/widgets/main_button.dart';
+import '../../../../core/resources/manager_color.dart';
+import '../../../../core/resources/managers_size.dart';
+
+class ChooseAccountView extends StatelessWidget {
+  const ChooseAccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ManagerColors.transparent,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.white10, Colors.black87, Colors.black87],
+              ),
+              image: DecorationImage(
+                opacity: 0.4,
+                image: AssetImage(
+                  ManagerAssets.chooseAccountPackgroundImage,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: ManagerHeight.h16, vertical: ManagerHeight.h60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  ManagerStrings.chooseAccountText1,
+                  style: getMediumTextStyle(
+                    fontSize: 14,
+                    color: ManagerColors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: ManagerHeight.h24,
+                ),
+                mainButton(
+                  color: ManagerColors.primaryColor,
+                  height: ManagerHeight.h48,
+                  minWidth: double.infinity,
+                  elevation: 0.1,
+                  shapeBorder: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text(
+                    ManagerStrings.signIn,
+                    style: getMediumTextStyle(
+                        fontSize: 14, color: ManagerColors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: ManagerHeight.h16,
+                ),
+                mainButton(
+                  color: ManagerColors.white,
+                  height: ManagerHeight.h48,
+                  minWidth: double.infinity,
+                  elevation: 0.1,
+                  shapeBorder: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text(
+                    ManagerStrings.signUp,
+                    style: getMediumTextStyle(
+                        fontSize: 14, color: ManagerColors.primaryColor),
+                  ),
+                ),
+                SizedBox(
+                  height: ManagerHeight.h24,
+                ),
+                mainButton(
+                  child: Text(
+                    ManagerStrings.chooseAccountText2,
+                    style: getMediumTextStyle(
+                      fontSize: 14,
+                      color: ManagerColors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
