db0f59b44993304aa61b626eef6ce6904af78477
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-06-03 19:39:03 +0300

    add new contents

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -3,5 +3,9 @@ const String jsonPath = "assets/json";
 
 class ManagerAssets {
   static const background = "$imagePath/background.png";
-  static const logo = "$imagePath/logo.svg";
+  static const logo = "$imagePath/logo.png";
+  static const logoPackground = "$imagePath/logo_packground.svg";
+  static const illustration1 = "$imagePath/illustration1.svg";
+  static const illustration2 = "$imagePath/illustration2.svg";
+  static const illustration3 = "$imagePath/illustration3.svg";
 }
--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,16 +1,16 @@
 class ManagerStrings {
   static const String noRouteFound = "No Route Found";
   static const String actHub = "Act-Hub";
-  static const String next = 'Next';
-  static const String startNow = 'Start Now';
-  static const String skip = 'Skip';
-  static const String outBoardingfirstTitle = "ميزة من مميزات التطبيق";
+  static const String next = 'التالي'; //'Next';
+  static const String startNow = 'إبدأ الآن'; //'Start Now';
+  static const String skip = 'تخطي';
+  static const String outBoardingfirstTitle = "أحجز مقعد في أقرب كافي لك";
   static const String outBoardingfirstBody =
       "وجب على المصمم أن يضع نصوصا مؤقتة على التصميم ليظهر للعميل الشكل كاملاً،دور مولد النص العربى";
-  static const String outBoardingSecondTitle = "ميزة من مميزات التطبيق";
+  static const String outBoardingSecondTitle = "شارك في التحديات وأربح";
   static const String outBoardingSecondBody =
       "وجب على المصمم أن يضع نصوصا مؤقتة على التصميم ليظهر للعميل الشكل كاملاً،دور مولد النص العربى";
-  static const String outBoardingThirdTitle = "ميزة من مميزات التطبيق";
+  static const String outBoardingThirdTitle = "طرق دفع متعددة وسهلة";
   static const String outBoardingThirdBody =
       "وجب على المصمم أن يضع نصوصا مؤقتة على التصميم ليظهر للعميل الشكل كاملاً،دور مولد النص العربى";
 }
