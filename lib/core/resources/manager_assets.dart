3af21333a7693085bc3790c6ba7c9851bdfb0f24
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-06-05 02:09:21 +0300

    add new assets for auth

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -8,6 +8,8 @@ class ManagerAssets {
   static const illustration1 = "$imagePath/illustration1.svg";
   static const illustration2 = "$imagePath/illustration2.svg";
   static const illustration3 = "$imagePath/illustration3.svg";
+  static const loginillustration = "$imagePath/logillustration.svg";
+  static const signupIllustration = "$imagePath/signupIllustration.svg";
   static const chooseAccountPackground =
       "$imagePath/choose_account_background.svg";
   static const chooseAccountPackgroundImage =
