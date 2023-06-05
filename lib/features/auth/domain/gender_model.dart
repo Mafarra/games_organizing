5a907d5336ae0897c89958cd68893b311abbb28d
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-06-06 01:40:00 +0300

    create new files

---
+++ lib/features/auth/domain/gender_model.dart
@@ -0,0 +1,7 @@
+class GenderModel {
+  String? genderType = '';
+  int? id;
+  String? svgPic;
+  bool? isChecked;
+  GenderModel({this.genderType, this.id, this.svgPic, this.isChecked});
+}
---
+++ lib/features/auth/presentaion/controller/registration_controller.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:games_organizing/core/extensions/extensions.dart';
+import 'package:games_organizing/core/resources/manager_assets.dart';
+import 'package:games_organizing/features/auth/domain/gender_model.dart';
+import 'package:get/get.dart';
+
+import '../../../../core/resources/manager_color.dart';
+import '../../../../routes/routes.dart';
+
+class RegisterController extends GetxController {
+  late TextEditingController fullName = TextEditingController();
+  late TextEditingController email = TextEditingController();
+  late TextEditingController password = TextEditingController();
+  late TextEditingController confirmPassword = TextEditingController();
+  late String genderType = '';
+  late String birthDate = '';
+  var formKey = GlobalKey<FormState>();
+  bool passwordVisible = false;
+  bool policyBoxValue = false;
+  Color genderBorderColor = Colors.grey;
+  List<GenderModel> genderItems = [
+    GenderModel(
+        genderType: 'ذكر', id: 1, svgPic: ManagerAssets.male, isChecked: false),
+    GenderModel(
+        genderType: 'أنثى',
+        id: 2,
+        svgPic: ManagerAssets.female,
+        isChecked: false),
+  ];
+  Icon showIconVisible() {
+    return Icon(
+      passwordVisible ? Icons.visibility : Icons.visibility_off,
+      color: ManagerColors.grey,
+    );
+  }
+
+  void changeIconVisibility() {
+    passwordVisible = !passwordVisible;
+    update();
+  }
+
+  void changePolicyBoxValue() {
+    policyBoxValue = !policyBoxValue;
+    update();
+  }
+
+  bool ischecked(GenderModel genderModel) {
+    return genderModel.isChecked.onNull();
+  }
+
+  void changedcheckedValue(GenderModel genderModel) {
+    genderModel.isChecked = !genderModel.isChecked.onNull();
+    saveGenderType(genderModel);
+    update();
+  }
+
+  changeBorderColorOnCheck(GenderModel genderModel) {
+    ischecked(genderModel)
+        ? genderBorderColor = ManagerColors.primaryColor
+        : genderBorderColor = ManagerColors.grey;
+    update();
+  }
+
+  void saveGenderType(GenderModel genderModel) {
+    genderType = genderModel.genderType.onNull();
+    update();
+  }
+
+  void clearCheckedgenders() {
+    genderItems[1].isChecked = false;
+    genderItems[0].isChecked = false;
+    update();
+  }
+
+  gotoLoginScreen() {
+    Get.offAllNamed(Routes.loginView);
+  }
+}
