7e79764d1553e770001d1195f9fc77b4fd6bf597
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-06-19 18:52:30 +0300

    create coffe model

---
+++ lib/features/game_reservation/domain/coffee_model.dart
@@ -0,0 +1,18 @@
+class CoffeeModel {
+  String? image;
+  String? coffeeName;
+  String? place;
+  int? price;
+  int? distance;
+  int? availableSeats;
+  bool? isopen;
+  CoffeeModel({
+    this.image,
+    this.coffeeName,
+    this.place,
+    this.price,
+    this.distance,
+    this.availableSeats,
+    this.isopen,
+  });
+}
